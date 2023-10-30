c34a0b24c12a73b148cddb883b324345ec072f97
Yaroslav Senkiv <slavko.senkiv@gmail.com>
2023-10-30 06:52:42 +0100

    add a few more modules, add suporting type modules, create basic express app, separate route handleing to separate file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('hello there');
+import express, { Request, Response} from 'express';
+import { router } from './routes/loginRoutes';
+
+const app = express();
+
+/* app.get('/', (req: Request, res: Response) => {
+  res.send(`
+    <div>
+      <h1> Hello there </h1>
+    </div>
+  `);
+});
+ */
+
+app.use(router);
+
+app.listen(3000, () => {
+  console.log('listening on port 3000');
+});
