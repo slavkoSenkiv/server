293db5a75273c2fe4873b5bcd742a079475872eb
Yaroslav Senkiv <slavko.senkiv@gmail.com>
2023-10-31 06:54:41 +0100

    add get and post methods

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,14 @@
 import express, { Request, Response} from 'express';
 import { router } from './routes/loginRoutes';
+import bodyParser from 'body-parser';
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 /* app.get('/', (req: Request, res: Response) => {
-  res.send(`
-    <div>
-      <h1> Hello there </h1>
-    </div>
-  `);
-});
- */
+  res.send(`    <div>      <h1> Hello there </h1>    </div>  `);
+}); */
 
 app.use(router);
 
